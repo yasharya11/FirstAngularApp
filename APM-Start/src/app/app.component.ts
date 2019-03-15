20f256e4a406da7b3698a25bed8098d8073eecb3
Yash <yasharya11@gmail.com>
2019-03-14 20:37:00 -0700

    updated my files

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,13 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template:`
-  <div>
-    <h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
-  </div>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+  title = 'Angular: Getting Started';
 }
